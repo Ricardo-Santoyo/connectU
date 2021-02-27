f88f1c8bb746c08b72ec72ddc8975dc3e8c0897e
Ricardo Santoyo <ricardosantoyoch@gmail.com>
2021-02-27 17:55:35 -0600

    added bio to ShowUser component

--- src/components/ShowUser.js
+++ src/components/ShowUser.js
@@ -1,7 +1,9 @@
 import React, { useEffect, useState } from 'react';
 import Header from './Header';
-//import defaultIcon from '../images/default-user-icon.jpg';
+import defaultIcon from '../images/default-user-icon.jpg';
 import getApiCall from '../apiCalls/getApiCall';
+import timeDifference from '../helperFunctions/timeDifference';
+import { Link } from 'react-router-dom';
 
 
 function ShowUser(props) {
@@ -13,12 +15,37 @@ function ShowUser(props) {
     .catch(error => error);
   }, [props.location.userID]);
 
+  function JoinedDate() {
+    let date = timeDifference(user.attributes.created_at, true);
+    return date.split(' · ')[1];
+  }
+
   return (
     <div className="Container">
 
       <Header title={user ? user.attributes.name : 'User'}/>
       {user ?
       <div id="ShowUser">
+        
+        <div className="Bio">
+          <div className="banner"></div>
+
+          <img src={defaultIcon} alt="User Icon" className="UserPhoto"></img>
+
+          <div className="UserInfo">
+            <h2>{user.attributes.name}</h2>
+            <span className="UserHandle">@{user.attributes.handle}</span>
+            <span className="UserHandle">Joined {JoinedDate()}</span>
+          </div>
+
+        </div>
+
+        <div className="ShowUserNav">
+          <Link to={{pathname: `${user.attributes.handle}`, userID: user.id}} replace={true} >Posts</Link>
+          <Link to={{pathname: `${user.attributes.handle}/replies`, userID: user.id}} replace={true} >Replies</Link>
+          <Link to={{pathname: `${user.attributes.handle}/media`, userID: user.id}} replace={true} >Media</Link>
+          <Link to={{pathname: `${user.attributes.handle}/likes`, userID: user.id}} replace={true} >Likes</Link>
+        </div>
       </div>
       : null}
     </div>
