c6d6838a905f7c1fa5104250e8553ec64c6fdb46
Ricardo Santoyo <ricardosantoyoch@gmail.com>
2021-05-13 15:12:52 -0700

    Comments are now visable on ShowPost pages

--- src/components/Comment.js
+++ src/components/Comment.js
@@ -1,10 +1,32 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
+import defaultIcon from '../images/default-user-icon.jpg';
+//import apiCall from '../apiCalls/apiCall';
+import CreatorLink from './CreatorLink';
+import InteractionOptions from './InteractionOptions';
 
 function Comment(props) {
+  const newTo = { 
+    pathname: `/${props.comment.user_handle}/comment/${props.comment.id}`, 
+    comment: props.comment
+  };
 
   return (
-    <div id="Comment">
-      {props.comment.body}
+    <div id="Post">
+
+      <Link to={`/${props.comment.user_handle}`}>
+        <img src={defaultIcon} alt='User Icon' className="PostUserIcon"></img>
+      </Link>
+
+      <div className="PostContent">
+        <CreatorLink data={props.comment}/>
+
+        <Link to={newTo}>
+          <p>{props.comment.body}</p>
+        </Link>
+
+        <InteractionOptions data={props.comment} />
+      </div>
     </div>
   );
 }
